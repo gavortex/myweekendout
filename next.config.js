ead9aad93e7a634ca55eb4789818963f99c94e99
gavortex <gavortex@yahoo.com>
2022-09-25 18:49:40 -0400

    commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+     typescript: {
+      ignoreBuildErrors: true,
+     }
+
+     
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['i.postimg.cc',
+      
+      'lh3.googleusercontent.com'],
+  }
+  
 }
 
 module.exports = nextConfig
