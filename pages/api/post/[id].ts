84a961b2a0552fe75da3f13807f62ba47e582ee3
Gary Alexis <gavortex@yahoo.com>
2025-04-13 17:49:10 -0400

    fix build Issues

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -7,7 +7,7 @@ import { uuid } from 'uuidv4';
 export default async function handler( req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     const { id } = req.query;
-    const query = postDetailQuery(id);
+    const query = postDetailQuery(id as string);
 
     const data = await client.fetch(query);
 
