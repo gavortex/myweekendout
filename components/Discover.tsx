42b518f0c9a9685007a54a651a940c068883ac33
Gary Alexis <gavortex@yahoo.com>
2025-04-14 16:26:46 -0400

    Sync latest changes for deployment

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -9,7 +9,7 @@ const Discover: NextPage = () => {
   const router = useRouter();
   const { topic } = router.query;
 
-  const activeTopicStyle = 'xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-white';
+  const activeTopicStyle = 'xl:border-2 hover:bg-red xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-white';
   const topicStyle = 'xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-red';
 
   return (
