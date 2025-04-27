e599d73727b740d072329af90775c9af90a57991
Gary Alexis <gavortex@yahoo.com>
2025-04-26 21:53:54 -0400

    Sync latest changes for deployment

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -74,19 +74,15 @@ const VideoCard: React.FC<IProps> = ({
     }
   }, [currentlyPlayingId, _id]);
 
-  useEffect(() => { 
-    if (videoRef?.current) {
-      videoRef.current.muted = isVideoMuted;
-      const video = videoRef.current;
-      if (video) {
-        video.muted = isVideoMuted;
-        if (!isVideoMuted && video.paused) {
-          video.play().catch((err) => console.log("Failed to play unmuted video:", err));
-        }
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video) {
+      video.muted = isVideoMuted;
+      if (!isVideoMuted && video.paused) {
+        video.play().catch((err) => console.log("Failed to play unmuted video:", err));
       }
     }
   }, [isVideoMuted]);
-  
 
   const addComment = async (e: React.FormEvent) => {
     e.preventDefault();
