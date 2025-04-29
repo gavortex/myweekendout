f738f3f00a2ab4afcbee5163697b133697e80fd1
Gary Alexis <gavortex@yahoo.com>
2025-04-29 14:37:57 -0400

    Update both frontend and backend

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -109,15 +109,15 @@ const VideoCard: React.FC<IProps> = ({
     const video = videoRef.current;
   
     if (isMobile && video) {
-      video.muted = false;
+      video.muted = true;
       video.playsInline = true;
   
-      // Wait a bit before trying to play, helps with render timing
-      setTimeout(() => {
-        video.play().catch((err) => {
-          console.log('Mobile autoplay prevented:', err);
-        });
-      }, 500);
+      const playPromise = video.play();
+      if (playPromise !== undefined) {
+        playPromise
+          .then(() => setPlaying(true))
+          .catch((err) => console.log('Autoplay prevented:', err));
+      }
     }
   }, []);
 
